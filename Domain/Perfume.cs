3362a404abc6e50b08ae2b132be18f59034782a0
Milan Stanisavljevic <stanisavljevic07@gmail.com>
2023-09-23 12:02:43 +0200

    add initial project structure

---
+++ Domain/Perfume.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Perfume
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public float Volume { get; set; }
+        public string Scent { get; set; }
+    }
+}
