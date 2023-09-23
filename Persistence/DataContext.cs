414ba65c0867304e8672a9638c8e6dbe2828a662
Milan Stanisavljevic <113298158+milan0161@users.noreply.github.com>
2023-09-23 14:40:37 +0200

    Add and configure DbContext. (#2)
    * Add and configure DbContext.
    
    * add seed data

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Bogus;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Perfume> Perfumes { get; set; }
+
+        public DataContext(DbContextOptions options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Perfume>(m =>
+            {
+                m.HasKey(e => e.Id);
+
+            });
+
+            var fakePerfumes = new List<Perfume>();
+            for (var i = 0; i < 20; i++)
+            {
+                var fakePerfume = new Faker<Perfume>()
+                    .RuleFor(x => x.Id, i + 1)
+                    .RuleFor(x => x.Name, f => f.Name.FirstName())
+                    .RuleFor(x => x.Brand, f => f.Company.CompanyName())
+                    .RuleFor(x => x.Volume, f => 110f)
+                    .RuleFor(x => x.ImageUrl, f => f.Image.LoremFlickrUrl())
+                    .RuleFor(x => x.Scent, f => f.Vehicle.Fuel());
+                fakePerfumes.Add(fakePerfume);
+            }
+
+            modelBuilder.Entity<Perfume>().HasData(fakePerfumes);
+        }
+    }
+}
---
+++ Persistence/PersistenceServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public static class PersistenceServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
